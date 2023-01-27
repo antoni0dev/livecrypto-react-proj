f1894ec4b4825712bf5e83472c5f5362934b815b
Antonio Mansory <antoniosimeonov7@gmail.com>
2023-01-27 08:09:44 +0200

    Add navbar functionality and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/Home'
+import SignIn from './routes/Signin'
+import SignUp from './routes/Signup'
+import CoinPage from './routes/CoinPage'
+import Account from './routes/Account'
 
 const App = () => {
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/coin/:coinId' element={<CoinPage />}>
+          <Route path=':coinId' />
+          </Route>
+        </Routes>
     </ThemeProvider>
   )
 }
